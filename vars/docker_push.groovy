bb0b5dc104ca14b29647aba5b3381ea936ad6c41
Ashish <ashish2024.ak@gmail.com>
2026-05-15 05:09:35 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -3,7 +3,6 @@ def call(String Project, String ImageTag, String dockerhubuser){
     echo "This is pushing image to docker hub repo"
     sh "docker login -u ${dockerhubuser} -p ${env.dockerHubPass}"
     sh "docker image tag ${dockerhubuser}/${Project}:${ImagTag}"
-    sh "docker push ${dockerhubuSer}/${Project}:${ImageTag}"
+    sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
         }
-    } 
-}
+    }
